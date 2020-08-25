808ac0386c9347eec3c7a446571dbc79d77e874c
Jon.D <60434382+jiyon-lee@users.noreply.github.com>
2020-08-26 08:10:17 +0900

    걸린시간 체크 기능 추가

--- cardflip.js
+++ cardflip.js
@@ -1,90 +1,98 @@
-var 가로 = 4;
-var 세로 = 3;
-var 색깔들 = ['red', 'red', 'orange', 'orange', 'yellow', 'yellow', 'green', 'green', 'white', 'white', 'pink', 'pink'];
-var 색깔후보 = 색깔들.slice();  // 참조관계 끊기 //백업, 초기화를 위해
-var 색깔 = [];
-var 클릭카드 = [];
-var 완성카드 = [];
-var 시작시간;
-var 클릭플래그 = true; //true일때만 클릭 가능하게
-function 셔플 () {
-  for (var i = 0; 색깔후보.length > 0; i += 1) {  //색깔후보에서 하나씩 뺼 때 색깔들도 바뀜,,참조관계 문
-    색깔 = 색깔.concat(색깔후보.splice(Math.floor(Math.random() * 색깔후보.length), 1));
-  }
+const 가로 = 3;
+const 세로 = 4;
+let 색깔 = [];
+let 이동값 = [];
+let 클릭카드 = [];
+let 완성카드 = [];
+let stopFlag = true; //트루일때만 클릭 가능하게
+const 색깔후보 = ['red', 'red', 'orange', 'orange', 'yellow', 'yellow', 'green', 'green', 'pink', 'pink', 'purple', 'purple']
+let startTime = [];
+let endTime;
+
+function 셔플() {
+    while (색깔후보.length > 0) {
+        이동값 = 색깔후보.splice(Math.floor(Math.random() * 색깔후보.length), 1)[0];
+        색깔.push(이동값);
+    }
 }
 
 function 카드세팅(가로, 세로) {
-  클릭플래그 = false;
-  for (var i = 0; i < 가로 * 세로; i += 1) {
-    var card = document.createElement('div');
-    card.className = 'card';
-    var cardInner = document.createElement('div');  //이건 html
-    cardInner.className = 'card-inner';             //이건 css
-    var cardFront = document.createElement('div');
-    cardFront.className = 'card-front';
-    var cardBack = document.createElement('div');
-    cardBack.className = 'card-back';
-    cardBack.style.backgroundColor = 색깔[i];
+    stopFlag = false;
+    for (let i = 0; i < 가로 * 세로; i++) { // 0 ~ 11 개의 카드 세팅
+        const card = document.createElement('div');
+        card.classList.add('card')
+        const cardInner = document.createElement('div');
+        cardInner.classList.add('card-inner')
+        const cardFront = document.createElement('div');
+        cardFront.classList.add('card-front')
+        const cardBack = document.createElement('div');
+        cardBack.classList.add('card-back')
+        cardBack.style.backgroundColor = 색깔[i];
+        cardInner.appendChild(cardFront);
+        cardInner.appendChild(cardBack);
+        card.appendChild(cardInner);
 
-    cardInner.appendChild(cardFront);
-    cardInner.appendChild(cardBack);
-    card.appendChild(cardInner);
+        //카드 맞추기 
+        (function (c) {
+            card.addEventListener('click', function () {
+                if (stopFlag && !완성카드.includes(c)) {
+                    c.classList.toggle('flipped');
+                    클릭카드.push(c);
 
-    //즉시실행함수로 감싸서 클로저 문제 해결하기.  card를 함수 안으로 넣기
-    (function (c) {
-      card.addEventListener('click', function() {
-        if (클릭플래그 && !완성카드.includes(c)) {
-          c.classList.toggle('flipped');
-          클릭카드.push(c);
-          if (클릭카드.length ===2) {
-            // console.log(클릭카드[0].querySelector('.card-back'), 클릭카드[0].querySelector('.card-back').style.backgroundColor);
-            // console.log(클릭카드[1].querySelector('.card-back'), 클릭카드[1].querySelector('.card-back').style.backgroundColor);
-            if (클릭카드[0].querySelector('.card-back').style.backgroundColor === 클릭카드[1].querySelector('.card-back').style.backgroundColor) {
-              완성카드.push(클릭카드[0]);
-              완성카드.push(클릭카드[1]);
-              클릭카드 = [];
-              if (완성카드.length === 12) {
-                var 끝시간 = new Date();
-                alert('축하합니다! 성공!' + (끝시간 - 시작시간) / 1000 + '초 걸렸습니다.');
-                document.querySelector('#wrapper').innerHTML = ''; // 내부태그 다 지우기 (낱개 지우기는 나중에)
-                색깔후보 = 색깔들.slice();
-                색깔 = [];
-                완성카드 = [];
-                시작시간 = null;
-                셔플();
-                카드세팅(가로, 세로);
-              }
-            } else { // 두개 색이 다르면
-              클릭플래그 = false;
-              setTimeout(function() {
-                클릭카드[0].classList.remove('flipped');
-                클릭카드[1].classList.remove('flipped');
-                클릭플래그 = true;
-                클릭카드 = [];
-              }, 1000);
-            }
-            // 클릭카드 = [];  //초기화하려고 했는데 여기에 쓰면 비동기보다 먼저 실행되니까 안됨 > 클릭카드[0]이 부존재
-          }
-        }
-      });
-    })(card);
-    document.querySelector('#wrapper').appendChild(card);
-  }
+                    let startTemp = Date.now();
+                    startTime.push(startTemp);
 
-  document.querySelectorAll('.card').forEach(function (card, index) {
-    setTimeout(function() {
-      card.classList.add('flipped');
-    }, 1000 + 100 * index  );
-  });
+                    //두번 클릭했을 때 
+                    if (클릭카드.length === 2) {
+                        //같은 색깔이면
+                        if (클릭카드[0].querySelector('.card-back').style.backgroundColor === 클릭카드[1].querySelector('.card-back').style.backgroundColor) {
+                            완성카드.push(클릭카드[0]);
+                            완성카드.push(클릭카드[1]);
+                            클릭카드 = [];
 
-  setTimeout(function() {
-    document.querySelectorAll('.card').forEach(function (card, index) {
-     card.classList.remove('flipped');
-   });
-   클릭플래그 = true;  //5초 뒤 세팅 끝났을 때
-   시작시간 = new Date();  //호출스택과 스코프 때문에 위로 따로 뻄
-  }, 5000);   // setTimeout의 괄호 끝에서 ms 넣어야함. 헷갈리지 말기
-};
+                            //다 눌렀을 때 초기화
+                            if (완성카드.length === 12) {
+                                endTime = Date.now();
+                                console.log(endTime);
+                                console.log(startTime);
+                                console.log(endTime - startTime[0]);
+                                startTime = [];
+                                endTime = [];
+                                완성카드 = [];
+                                document.querySelector('#wrapper').innerHTML = '';
+                                셔플();
+                                카드세팅(가로, 세로);
+                            }
+                            //다른 색깔이면
+                        } else {
+                            stopFlag = false;
+                            setTimeout(function () {
+                                클릭카드[0].classList.remove('flipped');
+                                클릭카드[1].classList.remove('flipped');
+                                클릭카드 = [];
+                                stopFlag = true;
+                            }, 1000);
+                        }
+                    }
+                }
+            });
+        })(card);
+        document.querySelector('#wrapper').appendChild(card);
+    };
+
+    //외울시간 주기
+    document.querySelectorAll('.card').forEach(function (card, i) {
+        setTimeout(function () {
+            card.classList.add('flipped');
+        }, 500 + 50 * i)
+    });
+    document.querySelectorAll('.card').forEach(function (card, i) {
+        setTimeout(function () {
+            card.classList.remove('flipped');
+            stopFlag = true;
+        }, 3000)
+    });
+}
 
 셔플();
 카드세팅(가로, 세로);
